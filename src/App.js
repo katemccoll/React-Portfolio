0744f7d06362ec61bc041cd9104bf04288dca976
Kate McColl <kate@katemccollart.com>
2021-07-22 12:19:47 +1200

    add: template

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+
+const App = () => <PortfolioContainer />;
+
+export default App;
