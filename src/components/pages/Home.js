00620e3848236b2eaebb0ca11bc46ae306ebc922
Kate McColl <kate@katemccollart.com>
2021-07-22 16:11:01 +1200

    change: header to home

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <section>
+                <h1>Kate McColl</h1>
+                <h1><span></span><span> </span></h1>
+            </section>
+        </div>
+    );
+}
+
+export default Home;
