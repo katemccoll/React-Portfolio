889319af4ca57a725db23375e219d649245e3bd5
Kate McColl <kate@katemccollart.com>
2021-07-22 12:28:19 +1200

    add: page change

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from "./Footer";
+import Project from "./pages/Project";
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Project') {
+            return <Project />;
+        }
+        return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+            <Footer />
+        </div>
+    );
+}
