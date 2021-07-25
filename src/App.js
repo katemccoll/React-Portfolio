49e88b684f97e9ba5ae26c16d1ce4cfbb52858ab
Kate McColl <kate@katemccollart.com>
2021-07-26 10:22:22 +1200

    add: library font to app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-
+import initFontAwesome from "./components/pages/assets/js/initFontAwesome";
+initFontAwesome();
 
 const App = () => <PortfolioContainer />;
 
