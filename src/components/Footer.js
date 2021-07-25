3bc449866b7947d2c0041563b87f6d0f03fa4d2a
Kate McColl <kate@katemccollart.com>
2021-07-26 10:22:03 +1200

    add: icon links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 
 function Footer() {
     return (
         <div>
+            <ul className="footer">
+                <li>
+                    <a className="footer-icon" href="https://github.com/katemccoll" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={['fab', 'github']} /></a>
+                </li>
+                <li>
+                    <a className="footer-icon" href="https://www.linkedin.com/in/kate-mccoll-a59389205/" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                    </a>
+                </li>
+                <li>
+                    <a className="footer-icon" href="https://twitter.com/kittykat6065059" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={['fab', 'twitter']} />
+                    </a>
+                </li>
 
+            </ul>
         </div>
     );
 }
