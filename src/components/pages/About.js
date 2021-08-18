fceeda25c3c4a0a955d71a262bcea6cd84fb7daa
Kate McColl <kate@katemccollart.com>
2021-08-18 17:05:09 +1200

    add: photos; update: about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,21 +3,23 @@ import "./assets/css/About.css";
 
 export default function About() {
     return (
-        <div id="flex">
+        <div id="about-container">
             <div id="about">
                 <div id="profile"></div>
             </div>
             <article id="about-me">
                 <p>
-                    Hello! I am Kate, a Kiwi that is currently living over in Melbourne. Since completing my Fine Arts
+                    Hello! I am Kate and I am born and raised in Dunedin, New Zealand. After completing my Fine Arts
                     degree at
-                    the University of Canterbury in 2016, I have been working in an Australian art and gift shop.
-                    While I enjoy the personal aspect of my job, it drains a lot of my artistic ambition, so when I come
-                    home I
-                    often want to get away from art.
+                    the University of Canterbury in 2016, I moved to Melbourne. While there, I worked at an Australian art and gift shop.
+                    While I enjoyed the personal aspect of my job, it drained a lot of my artistic ambition, so when I came
+                    home I often want to get away from art.
+
+                    
                     A career as a web developer would be great because I'd be able to focus on solving interesting
                     problems
                     during the day, while still having the energy and desire to create my art outside of work.
+                    I have now moved back to New Zealand, but the great thing about web development is that you can work anywhere in the world!
                 </p>
 
                 <p>
@@ -32,15 +34,13 @@ export default function About() {
 
                 </p>
                 <p>
-                    I am currently enrolled at Monash University Coding Bootcamp, very intense and fast pace but enjoy
-                    learning and thinking about websites in a different way. I have really enjoyed creating websites
+                    I have graduated from Monash University Coding Bootcamp, which was a very intense, fast paced but enjoyable course. I loved
+                    learning to think about websites in a different way. I really enjoyed creating websites
                     from
-                    scratch and looking for the small details to make the page better.
-                    I love that I can bring my creative side to each of my assignments and using my knowledge to build a
-                    site
-                    like a jigsaw puzzle. I hope by the end of this course, I will have the confidence to build and
-                    create
-                    innovate websites and bring my art skills into the digital art world.
+                    scratch and love looking for the small details to make the page better.
+                    I love that I can bring my creative side to the work and use my knowledge to build a
+                    site, just
+                    like a jigsaw puzzle.
 
                 </p>
             </article>
