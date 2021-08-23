c502db07f1b482af3496c95e1ee71dcfc5e84ce4
Kate McColl <kate@katemccollart.com>
2021-08-24 09:59:01 +1200

    add: hide nav when scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,22 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
     const [click, setClick] = useState(false);
+    const [scroll, setScroll] = useState(false);
 
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false);
+
+
+    const hideNav = () => {
+        if (window.scrollY >= 300) {
+            setScroll(true);
+        } else {
+            setScroll(false);
+        }
+    };
+
+    window.addEventListener('scroll', hideNav);
 
     return (
         <nav className="navbar">
@@ -19,27 +31,27 @@ const Navbar = () => {
             </div>
                 <ul className={click ? 'navbar-menu active' : "navbar-menu"}>
                     <li className="nav-item">
-                        <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                        <Link to="/" className={scroll ? "nav-link active" : "nav-link"} onClick={closeMobileMenu}>
                             Home
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/about" className="nav-link" onClick={closeMobileMenu}>
+                        <Link to="/about" className={scroll ? "nav-link active" : "nav-link"} onClick={closeMobileMenu}>
                             About
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/projects" className="nav-link" onClick={closeMobileMenu}>
+                        <Link to="/projects" className={scroll ? "nav-link active" : "nav-link"} onClick={closeMobileMenu}>
                             Projects
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/resume" className="nav-link" onClick={closeMobileMenu}>
+                        <Link to="/resume" className={scroll ? "nav-link active" : "nav-link"} onClick={closeMobileMenu}>
                             Resume
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/contact" className="nav-link" onClick={closeMobileMenu}>
+                        <Link to="/contact" className={scroll ? "nav-link active" : "nav-link"} onClick={closeMobileMenu}>
                             Contact
                         </Link>
                     </li>
