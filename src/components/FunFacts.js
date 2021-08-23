0d008cc24527c3cd8b82527a4d47d4ccc7eee079
Kate McColl <kate@katemccollart.com>
2021-08-24 09:58:49 +1200

    add: padding for facts

--- src/components/FunFacts.js
+++ src/components/FunFacts.js
@@ -6,7 +6,7 @@ const FunFacts = () => {
 
     return (
         <div className="facts-container">
-            <div className="personality-container">
+            <div className="single-fact-container">
                 <h2>Just a little bit about me...</h2>
                 <p>
                     Best known for always having a smile on my face and a bubbly personality to match.
@@ -14,7 +14,7 @@ const FunFacts = () => {
                     I love to learn!
                 </p>
             </div>
-            <div className="artist-container">
+            <div className="single-fact-container">
                 <h2>Artist</h2>
                 <p>
                     I have been drawn to art from a young age, always interested in emotion and
@@ -25,7 +25,7 @@ const FunFacts = () => {
                     Though I still paint and draw, I am now experimenting with digital art.
                 </p>
             </div>
-            <div className="contact-me-container">
+            <div className="single-fact-container">
                 <h2>
                     Want to work together?
                 </h2>
