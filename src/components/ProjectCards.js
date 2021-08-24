8a1d4d2217ede3ec689b0072c1b4845fc717d71e
Kate McColl <kate@katemccollart.com>
2021-08-24 17:59:59 +1200

    add: project cards

---
+++ src/components/ProjectCards.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./styles/ProjectCards.css";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import WorkDay from "./pages/assets/images/portfolio/work_day.JPG";
+
+
+const ProjectCards = () => {
+    return (
+        <div className="project-card-container">
+
+            <div className="TB project-image">
+                <h3 className="featured-title">
+                    Tech Blog
+                </h3>
+                <ul className="tech-list">
+                    <li>Node</li>
+                    <li>Handlebars</li>
+                    <li>Sequelize</li>
+                </ul>
+                <div className="project-icon">
+                    <a href="https://hidden-retreat-02954.herokuapp.com/dashboard"
+                       target="_blank" rel="noreferrer" className="icon-link">
+                        <FontAwesomeIcon icon={["fas", "link"]} />
+                    </a>
+                    <a href="https://github.com/katemccoll/Tech-Blog" target="_blank" rel="noreferrer" className="icon-github">
+                        <FontAwesomeIcon icon={['fab', 'github']} />
+                    </a>
+                </div>
+            </div>
+
+            <div className="project-image NT">
+                <h3 className="featured-title">Note Taker</h3>
+                <ul className="tech-list">
+                    <li>Node</li>
+                    <li>Bootstrap</li>
+                    <li>Local Storage</li>
+                </ul>
+                <div className="project-icon">
+                    <a href="https://secret-oasis-60851.herokuapp.com/"
+                       target="_blank" rel="noreferrer" className="icon-link">
+                        <FontAwesomeIcon icon={["fas", "link"]} />
+                    </a>
+                    <a href="https://github.com/katemccoll/Note-Taker" target="_blank" rel="noreferrer" className="icon-github">
+                        <FontAwesomeIcon icon={['fab', 'github']} /></a>
+                </div>
+            </div>
+
+            <div className="project-image WD">
+                <h3 className="featured-title">Weather Dashboard</h3>
+                <ul className="tech-list">
+                    <li>jQuery</li>
+                    <li>API</li>
+                    <li>Local Storage</li>
+                </ul>
+                <div className="project-icon">
+                    <a href="https://katemccoll.github.io/Weather_Dashboard/" target="_blank" rel="noreferrer" className="icon-link">
+                        <FontAwesomeIcon icon={["fas", "link"]} /></a>
+                    <a href="https://github.com/katemccoll/Weather_Dashboard" target="_blank" rel="noreferrer" className="icon-github">
+                        <FontAwesomeIcon icon={['fab', 'github']} />
+                    </a>
+                </div>
+            </div>
+
+            <div className="project-image DP">
+                <h3 className="featured-title">Day Planner</h3>
+                <ul className="tech-list">
+                    <li>jQuery</li>
+                    <li>Bootstrap</li>
+                    <li>Local Storage</li>
+                </ul>
+                <div className="project-icon">
+                    <a href="https://katemccoll.github.io/Work_Day_Scheduler/Develop/index.html"
+                       target="_blank" rel="noreferrer" className="icon-link"><FontAwesomeIcon icon={["fas", "link"]} /></a>
+                    <a href="https://github.com/katemccoll/Work_Day_Scheduler" target="_blank" rel="noreferrer" className="icon-github">
+                        <FontAwesomeIcon icon={['fab', 'github']} />
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    )
+};
+
+export default ProjectCards;
