2cc08ac45cd348a7c2463abaeaae2e238c77e4dd
Kate McColl <kate@katemccollart.com>
2021-09-04 11:29:23 +1200

    fix: routing home page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Router>
             <Navbar />
             <Switch>
-                <Route path='/' exact component={Home} />
+                <Route path='/' component={Home} />
                 <Route path='/about' component={About} />
                 <Route path='/projects' component={Projects} />
                 <Route path='/contact' component={Contact} />
